ab75d252498cd8951a1f38ae16641bd0730e10db
almiche <chamich196@hotmail.com>
2020-12-28 20:14:18 -0500

    Clean up the interface and inject recipes externally

--- src/main/java/Meals.java
+++ src/main/java/Meals.java
@@ -8,32 +8,25 @@ import com.fasterxml.jackson.databind.type.MapType;
 
 public class Meals {
 
-    private static HashMap<Integer,String> recipes = new HashMap<Integer,String>();
+    private HashMap<Integer,String> recipes = new HashMap<Integer,String>();
 
-    private static Map<String, String> recipes() throws IOException {
-        String fileName = "src/main/resources/recipes.json";
-        String recipesJson = new String(Files.readAllBytes(Paths.get(fileName).toAbsolutePath()));
-        final ObjectMapper mapper = new ObjectMapper();
-        final MapType type = mapper.getTypeFactory().constructMapType(
-                HashMap.class, String.class, String.class);
-        final HashMap<String, String> data = mapper.readValue(recipesJson, type);
-        return data;
+    public Meals(HashMap<String,String> customRecipes) {
+        processRecipes(customRecipes);
     }
 
-    private static HashMap<Integer,String> allCombos() throws IOException {
-        recipes().forEach((ingredients,recipe)-> {
-             recipes.put(new HashSet<String>(
-                     Arrays.asList(
-                             ingredients.split(
-                                     ""
-                             )
-                     )
-             ).hashCode(),recipe);
-        });
-        return recipes;
+    public Meals(String recipePath) throws IOException {
+        processRecipes(loadRecipes(recipePath));
     }
 
-    public static String mealify(String ingredients) throws IOException {
+    public Meals() throws IOException {
+        processRecipes(
+            loadRecipes(
+                "src/main/resources/recipes.json"
+            )
+        );
+    }
+
+    public String mealify(String ingredients) throws IOException {
         Integer ingredientHash = new HashSet<String>(
                 Arrays.asList(
                         ingredients.split(
@@ -41,6 +34,27 @@ public class Meals {
                         )
                 )
         ).hashCode();
-        return allCombos().get(ingredientHash);
+        return recipes.get(ingredientHash);
+    }
+
+    private HashMap<String, String> loadRecipes(String filename) throws IOException {
+        String recipesJson = new String(Files.readAllBytes(Paths.get(filename).toAbsolutePath()));
+        ObjectMapper mapper = new ObjectMapper();
+        MapType type = mapper.getTypeFactory().constructMapType(
+                HashMap.class, String.class, String.class);
+        HashMap<String, String> data = mapper.readValue(recipesJson, type);
+        return data;
+    }
+
+    private void processRecipes(HashMap<String, String> unprocessedRecipes) {
+        unprocessedRecipes.forEach((ingredients,recipe)-> {
+            recipes.put(new HashSet<String>(
+                    Arrays.asList(
+                            ingredients.split(
+                                    ""
+                            )
+                    )
+            ).hashCode(),recipe);
+        });
     }
 }
--- src/test/java/MealsTest.java
+++ src/test/java/MealsTest.java
@@ -2,18 +2,48 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class MealsTest {
 
+    private static Meals MEALS;
+
+    static {
+        try {
+            MEALS = new Meals();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     public void TestItWorks() throws IOException {
-        String result = Meals.mealify("🐟🍚");
+        String result = MEALS.mealify("🐟🍚");
         Assert.assertEquals(result,"🍣");
     }
 
     @Test
     public void TestItWorksTheOtherWayAround() throws IOException {
-        String result = Meals.mealify("🍚🐟");
+        String result = MEALS.mealify("🍚🐟");
         Assert.assertEquals(result,"🍣");
+    }
+
+    @Test
+    public void TestItWorksWithCustomPath() throws IOException {
+        Meals newMeals = new Meals("src/main/resources/recipes.json");
+        String result = newMeals.mealify("🍚🐟");
+        Assert.assertEquals(result, "🍣");
+    }
+
+    @Test
+    public void TestItWorksWithCustomRecipes() throws IOException {
+        HashMap<String,String> customRecipes = new HashMap<>();
+        customRecipes.put("🥕⛏🤮","🦅");
+        customRecipes.put("🎄","💻");
+        customRecipes.put("🎅🏻","🍚");
+
+        Meals newMeals = new Meals(customRecipes);
+        String result = newMeals.mealify("🎅🏻");
+        Assert.assertEquals(result, "🍚");
     }
 }
